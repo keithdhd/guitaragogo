1b27818b130e0e46db7d9440fa61a7563695029b
keithdhd <keith@keithdouglas.co.uk>
2015-08-03 11:37:06 +0100

    create user controller

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,5 @@
 class Song < ActiveRecord::Base
+  has_and_belongs_to_many :genres
+  has_many :lessons
+  belongs_to :saved_song
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  - has_many :saved_songs, class_name:'SavedSong', foreign_key:student_id
-  - has_many :lessons, class_name:'Lesson', foreign_key:teacher_id
-  - has_many :songs, through :saved_songs
+  has_many :saved_songs, class_name:'SavedSong', foreign_key:student_id
+  has_many :lessons, class_name:'Lesson', foreign_key:teacher_id
+  has_many :songs, through :saved_songs
 end
