de198cee1d6b2f615d649f7b2cb65192a4c80fa1
keithdhd <keith@keithdouglas.co.uk>
2015-08-04 14:54:58 +0100

    add to sved songs link

--- app/controllers/saved_songs_controller.rb
+++ app/controllers/saved_songs_controller.rb
@@ -7,10 +7,24 @@ class SavedSongsController < ApplicationController
   end
 
   def create
-    @saved_song = SavedSong.new(params["saved_song"])
+    @saved_song = SavedSong.new(song_id: params[:song_id])
+    @saved_song.student_id = current_user.id
+    @saved_song.status = "To do"
     @saved_song.save
+    redirect_to saved_songs_path
   end
 
   def destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_saved_song
+    @saved_song = SavedSong.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def saved_song_params
+    params.require(:saved_songs).permit(:student_id, :song_id)
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,18 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+    # @saved_song = SavedSong.all
+    # @saved_song.save
+
+    # respond_to do |format|
+    #   if @saved_song.save
+    #     format.html { redirect_to login_path, notice: 'Sweet! Good work. You created a new lesson' }
+    #     format.json { render :show, status: :created, location: lessons_path }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @lesson.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   def new
