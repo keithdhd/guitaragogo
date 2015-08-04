6152158bb2f5c6e80d661c5f91498b368d823b60
keithdhd <keith@keithdouglas.co.uk>
2015-08-04 13:49:28 +0100

    add Lesson form

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,9 +4,23 @@ class LessonsController < ApplicationController
   end
 
   def new
+    @lesson = Lesson.new
   end
 
   def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.instrument_id = 1
+    @lesson.teacher_id = current_user.id
+
+    respond_to do |format|
+      if @lesson.save
+        format.html { redirect_to login_path, notice: 'Sweet! Good work. You created a new lesson' }
+        format.json { render :show, status: :created, location: lessons_path }
+      else
+        format.html { render :new }
+        format.json { render json: @lesson.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -17,5 +31,16 @@ class LessonsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lesson
+      @lesson = Lesson.find(params[:id])
+    end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def lesson_params
+    params.require(:lesson).permit(:teacher_id, :title, :video_url, :intrument_id, :song_id)
   end
 end
